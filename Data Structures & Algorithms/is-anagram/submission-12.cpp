ec6c179d5b1b8e806649883ff9712d5624c87396
Max Felix Eriksen Storteig <maxflaks123@gmail.com>
2026-09-24 12:26:02 +0200

    Add: is-anagram - submission-12

---
+++ Data Structures & Algorithms/is-anagram/submission-12.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        // Create map for string s and t with frequency
+        if(s.length() != t.length()) {
+            return false;
+        }
+        map<char, int> count = createCount(s);
+        for(char c : t) {
+            if(!count.contains(c)) {
+                return false;
+            }
+            count[c] -= 1;
+            if(count[c] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    map<char, int> createCount(string_view s) 
+    {
+        map<char, int> count;
+        for(char c : s) 
+        {
+            if(!count.contains(c))
+            {
+                count.insert({c, 1});
+            }
+            else 
+            {
+                count[c] = count[c] +1;
+            }
+        }
+        return count;
+    }
+};
