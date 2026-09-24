caf0be291f05ec6fa7f376e0ddd40cf65a421301
Max Felix Eriksen Storteig <maxflaks123@gmail.com>
2026-09-24 12:50:17 +0200

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(int i{0}; i < nums.size(); ++i) 
+        {
+            int x { target - nums[i]};
+            for(int j{i+1}; j < nums.size(); ++j)
+            {
+                if(x == nums[j]) 
+                {
+                    vector<int> res = {i,j};
+                    return res;
+                }
+            }            
+        }
+        // not reachable by task definition
+        vector<int> res = {-1, -1};
+        return res;
+    }
+};
